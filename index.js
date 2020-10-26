5ec2f68f73436213878528f66cf07b6150cb35fe
rarwrly <ehc2642@gmail.com>
2020-10-25 23:32:16 -0400

    updates for the review forms

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const getReviews = async()=>{
 const displayResturants = (resturants,reviews)=>{
     
     const result = document.querySelector(".results");
-
+    result.innerHTML =""
     
     const resturantPosts = resturants.map(resturant=>{
         
@@ -28,19 +28,75 @@ const displayResturants = (resturants,reviews)=>{
             const reviewDivs = resturantReviews.map((review)=>{
                     return `<div class ="review"><div class ="rating">Stars:${review.stars}</div><div class="reviewText">Review:${review.text}</div></div>`
                 });
-
-         return `<div class ="resturant">
-<div class ="name"><h1>${resturant.name}</h1></div>
-<div class ="rating">Avg Stars:${resturant.avgRating}</div
-div class ="address"> Address: ${resturant.address}</div>
-<div class ="img"><img src ="${resturant.imgUrl}"></div>
-<div class ="reviewsContainer">${reviewDivs.join("")}</div>
-</div>`;
+           const form = document.createElement("div");
+                form.classList.add("formContianer")
+                form.innerHTML+=`
+                <form id ="formId_${resturant.id}">
+                <input type="hidden" name ="resturantId" value="${resturant.id}">
+                Rating: <input type="text" name="rating"><br>
+                Review: <input type="text" name="review"><br>
+                <input type="button" onclick="createReviewFromForm('formId_${resturant.id}')" value="Submit">
+                </form>`;
+        
+            const resturantContainer = document.createElement("div");
+                resturantContainer.classList.add("resturantContainer");
+                resturantContainer.innerHTML += `
+                    <div class ="name"><h1>${resturant.name}</h1></div>
+                    <div class ="rating">Avg Stars:${resturant.avgRating}</div>
+                    <div class ="address"> Address: ${resturant.address}</div>
+                    <div class ="img"><img src ="${resturant.imgUrl}"></div>
+                    <div class ="reviewsContainer" id ="resturantId_${resturant.id}">${reviewDivs.join("")}</div>`;
+                resturantContainer.append(form);
+         return resturantContainer;
     });
     
-    result.innerHTML += resturantPosts.join("");
-
+resturantPosts.forEach (resturant=>{
+    result.append(resturant);
+});
 };
+
+function createReviewFromForm(formId){
+    const formDetails= document.getElementById(formId)
+    const formData= new FormData(formDetails)
+    const restID = parseInt(formData.get ("resturantId"));
+    const stars = parseInt(formData.get ("rating"));
+    const text = formData.get ("review")
+    console.log(restID)
+    console.log (stars)
+    console.log(text)
+    
+
+  const newReview = {
+    restaurantId: restID,
+    stars: stars,
+    text: text,
+  };
+
+    createReview(newReview);
+};
+    
+const createReview = async (newReview) => {
+  // POST request - create a record in a database
+  await fetch("http://localhost:3000/reviews", {
+    method: "POST",
+    body: JSON.stringify(newReview),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+      console.log("success");
+    showReviews();
+};
+
+
+
+//displayReviews.addEventListener("click",()=>{
+// check if active class is applied to any other review, if yes then set to inactice
+// if the resturant_Id contains the resturantId 
+// element.classList.remove("inactive");
+//  element.classList.add("active");
+//});
 
 const showReviews = async ()=>{
     try{
@@ -65,18 +121,3 @@ const showReviews = async ()=>{
 };
 
 showReviews();
-
-
-
-        /*
-        resturants.forEach((resturant)=>{
-            const resturantReviews = reviews.filter(review => {
-                return review.restaurantId == resturant.id
-            });
-            console.log(resturantReviews)
-            const ratings = resturantReviews.map(review => review.stars);
-            const avgRating = ratings.reduce((a,b) => a + b, 0) / ratings.length;
-            //two lines above could be condensesd?
-            resturant.avgRating = avgRating
-        });
-        */
